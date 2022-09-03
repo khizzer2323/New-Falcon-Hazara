29bb8dc05d4d2515211d5cafd102bb8536f06e88
khizzer2323 <hizarabbasi007@gmail.com>
2022-09-03 13:50:02 +0500

    released,added sounds and animations

---
+++ index.js
@@ -0,0 +1,20 @@
+
+
+//!!!!!!!!!!!!!!!!!Code for Light Mode !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+function lightMode() {
+    // Main page code dark and light
+
+    $('#black').toggleClass('black');
+    $('#black').toggleClass('.light');
+
+    //maps page dark light mode
+    $('#black1').toggleClass('.black');
+    $('#black1').toggleClass('light');
+
+    $('#icon').toggleClass('iconlight');
+    $('#icon1').toggleClass('iconlight');
+    $('#icon2').toggleClass('iconlight');
+}
+
+// Code for light mode ends ^ !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
